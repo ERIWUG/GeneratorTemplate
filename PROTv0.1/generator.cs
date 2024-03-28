7b708efc751b528b1e671cc7ffff4da8e326a8d6
Sasha Veremeychik <veremeychik-s@mail.ru>
2024-03-28 14:16:11 +0300

    Добавил метод с комбинацией

--- PROTv0.1/generator.cs
+++ PROTv0.1/generator.cs
@@ -253,4 +253,157 @@ namespace PROTv0._1
             Console.WriteLine();
         }
     }
+
+    //моё_!_!_!_!_!_!_!_!_!_!_!__!!__!_!_!_!_!_!__!_!_!__!_!__!_!__!_!_!__!_!_!_!__!_!_!_!_!_!__!
+   
+    public static void GenerateGroup(MyData[] mas, int ogr, int amount)
+    {
+        Random rand = new Random();
+        int amQuest = 0;
+        List<int> intTrueAns = new List<int>();
+        List<int> intFalseAns = new List<int>();
+        List<int> intQuest = new List<int>();
+        List<int> intAnswer = new List<int>();
+
+        List<string> AllAnsw = new List<string>();
+        List<string> CorrectAnswers = new List<string>();
+        List<string> GroupOfAnswers = new List<string>();
+
+        void ParseData(MyData[] mas)
+        {
+            int i = -1;
+            while (i++ < mas.Length - 1)
+            {
+
+                if (mas[i].priz == 1)
+                {
+                    intQuest.Add(i);
+                }
+                else
+                {
+                    intAnswer.Add(i);
+                    if (mas[i].isTrue)
+                    {
+                        intTrueAns.Add(i);
+                    }
+                    else
+                    {
+                        intFalseAns.Add(i);
+                    }
+                }
+            }
+        }
+
+
+        string GenerateRandomStrings(List<string> list)
+        {
+            // Создание копии списка для извлечения элементов без повторений
+            List<string> tempList = new List<string>(list);
+            List<string> randomElements = new List<string>();
+
+            // Определение случайного количества элементов для выборки
+            int numberOfElements = rand.Next(0, tempList.Count + 1);
+            if (numberOfElements == 0)
+            {
+                return "Ничего из перечисленного";
+            }
+            // Выбор случайных элементов без повторений и в исходном порядке
+            while (randomElements.Count < numberOfElements)
+            {
+                int index = rand.Next(tempList.Count);
+                randomElements.Add(tempList[index]);
+                tempList.RemoveAt(index); // Удаление выбранного элемента, чтобы избежать повторений
+            }
+            randomElements.Sort((a, b) => list.IndexOf(a).CompareTo(list.IndexOf(b)));
+            return String.Join("; ", randomElements);
+        }
+
+
+
+
+        void GenerateAnswers(List<int> full,bool sign, int k)
+        {
+            while (k-- > 0)
+            {
+                int IA = rand.Next(full.Count);
+                var AA = mas[full[IA]];
+                full.RemoveAt(IA);
+                Console.WriteLine($"----){AA.text},  {AA.isTrue}\n");
+                AllAnsw.Add(AA.text);
+                if (sign)
+                {
+                    if (AA.isTrue)
+                    {
+                        CorrectAnswers.Add(AA.text);
+                    }
+                }
+                else
+                {
+                    if (!AA.isTrue)
+                    {
+                        CorrectAnswers.Add(AA.text);
+                    }
+                }
+            }
+            string CorrectString = String.Join("; ", CorrectAnswers);
+
+            int minvalue = 3;
+            int maxvalue = 5;
+            if (AllAnsw.Count == 3)
+            {
+                maxvalue = 4;
+            }
+            int NumberOfAnswers = rand.Next(minvalue, maxvalue);
+
+            GroupOfAnswers.Add(CorrectString);
+            while (GroupOfAnswers.Count < NumberOfAnswers)
+            {
+                string randomString = GenerateRandomStrings(AllAnsw);
+                if (!GroupOfAnswers.Contains(randomString))
+                {
+                    GroupOfAnswers.Add(randomString);
+                }
+
+
+            }
+            foreach (string str in GroupOfAnswers)
+            {
+                Console.WriteLine(str);
+            }
+            GroupOfAnswers.Clear();
+        }
+
+
+        ParseData(mas);
+
+        while (amount-- > 0)
+        {
+            Console.WriteLine($"{amount}");
+            List<int> mT = intTrueAns;
+            List<int> mF = intFalseAns;
+            List<int> Answers = intAnswer;
+            int k = rand.Next(2, ogr);
+
+            int IQ = rand.Next(intQuest.Count);
+            var AQ = mas[intQuest[IQ]];
+            intQuest.RemoveAt(IQ);
+            AllAnsw.Clear();
+            CorrectAnswers.Clear();
+
+            Console.WriteLine($"{AQ.text}\n");
+            int ppp = 0;
+            if (AQ.isNeg)
+            {
+                GenerateAnswers(Answers, false, k);
+            }
+            else
+            {
+                GenerateAnswers(Answers, true, k);
+            }
+
+
+        }
+
+    }
 }
+
